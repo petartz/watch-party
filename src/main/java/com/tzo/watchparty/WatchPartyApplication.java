81ebaf8a06a23220aac0ff14c4384fd2e07b2a91
him <ptzonevski@gmail.com>
2025-05-24 19:12:46 -0400

    simple starter

---
+++ src/main/java/com/tzo/watchparty/WatchPartyApplication.java
@@ -0,0 +1,13 @@
+package com.tzo.watchparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchPartyApplication.class, args);
+	}
+
+}
